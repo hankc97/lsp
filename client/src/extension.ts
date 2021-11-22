c1845e089f24d7bd7e610dae87475fa07cf99483
Hank <hankchen1997@yahoo.com>
2021-11-22 11:25:14 -0500

    added logger and renamed file directory to client/server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext, TextDocument } from 'vscode';
+import { workspace, ExtensionContext, TextDocument, Disposable } from 'vscode';
 import * as net from 'net';
 import {
 	LanguageClient,
@@ -14,76 +14,53 @@ import {
     StreamInfo,
 } from 'vscode-languageclient';
 
-
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	// const serverModule = context.asAbsolutePath(
-	// 	path.join('server', 'out', 'server.js')
-	// );
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	// const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-
     console.log("starting client...");
-    // The server is a started as a separate app and listens on port 5007
-    const connectionInfo = {
-        port: 5007
+
+    const executable = workspace.getConfiguration("plaintext").get;
+    console.log(executable);
+
+    context.subscriptions.push(startLanguageServerTCP(5007, ["plaintext"]));
+}
+
+
+function startLanguageServer(command: string, args: string[], documentSelector: string[]): Disposable {
+    const serverOptions: ServerOptions = {
+        command, 
+        args,
     };
-    const serverOptions = () => {
-        // Connect to language server via socket
-        const socket = net.connect(connectionInfo);
-        const result: StreamInfo = {
-            writer: socket,
-            reader: socket
-        };
-        return Promise.resolve(result);
-    };
-	// const serverOptions: ServerOptions = {
-	// 	run: { module: serverModule, transport: TransportKind.ipc },
-	// 	debug: {
-	// 		module: serverModule,
-	// 		transport: TransportKind.ipc,
-	// 		options: debugOptions
-	// 	}
-	// };
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/*.*')
-		},
-		middleware: {
-			didOpen: (document, next) => {
-                console.log("didOpen", document);
-                return next(document);
-			},
-			provideCompletionItem: async (document, position, context, token, next) => {
-				// If not in `<style>`, do not perform request forwarding
-				console.log(context);
-				console.log(document);
-				return await next(document, position, context, token);
-			},
-		}
-	};
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: documentSelector,
+        synchronize: {
+            configurationSection: "plaintext"
+        },
+    }
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
+    return new LanguageClient(
+        command, 
+        serverOptions,
+        clientOptions
+    ).start()
+} 
 
-	// Start the client. This will also launch the server
-	client.start();
+function startLanguageServerTCP(address: number, documentSelector: string[]): Disposable {
+    const serverOptions: ServerOptions = () => {
+        return new Promise((resolve, reject) => {
+            const client = new net.Socket();
+            client.connect(address, "127.0.0.1", () => {
+                resolve({reader: client, writer: client});
+            })
+        })
+    }
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: documentSelector,
+    }
+
+    return new LanguageClient(`tcp language server (port ${address})`, serverOptions, clientOptions).start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -92,3 +69,61 @@ export function deactivate(): Thenable<void> | undefined {
 	}
 	return client.stop();
 }
+
+
+
+
+
+
+
+// // The server is a started as a separate app and listens on port 5007
+//     // const connectionInfo = {
+//     //     port: 5007,
+//     //     host: "127.0.0.1",
+//     //     (connectListener) => {
+//     //     }
+//     // };
+//     const serverOptions = async () => {
+//         // Connect to language server via socket
+//         const socket = net.connect({port: 5007}, () => { //'connect' listener
+//           console.log('connected to server!');
+//         });
+//         const result: StreamInfo = {
+//             writer: socket,
+//             reader: socket
+//         };
+//         return await Promise.resolve(result);
+//     };
+
+// 	// Options to control the language client
+// 	const clientOptions: LanguageClientOptions = {
+// 		// Register the server for plain text documents
+// 		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+// 		synchronize: {
+// 			// Notify the server about file changes to '.clientrc files contained in the workspace
+// 			fileEvents: workspace.createFileSystemWatcher('**/*.*')
+// 		},
+// 		middleware: {
+// 			didOpen: (document, next) => {
+//                 console.log("didOpen", document);
+//                 return next(document);
+// 			},
+// 			provideCompletionItem: async (document, position, context, token, next) => {
+// 				// If not in `<style>`, do not perform request forwarding
+// 				console.log(context);
+// 				console.log(document);
+// 				return await next(document, position, context, token);
+// 			},
+// 		}
+// 	};
+
+// 	// Create the language client and start the client.
+// 	client = new LanguageClient(
+// 		'languageServerExample',
+// 		'Language Server Example',
+// 		serverOptions,
+// 		clientOptions
+// 	);
+
+// 	// Start the client. This will also launch the server
+// 	client.start();
--- go.mod
+++ go.mod
@@ -8,26 +8,39 @@ require (
 	github.com/sourcegraph/go-lsp v0.0.0-20200429204803-219e11d77f5d
 	github.com/sourcegraph/jsonrpc2 v0.1.0
 	github.com/stretchr/testify v1.7.0
-	go.lsp.dev/jsonrpc2 v0.9.0
-	go.lsp.dev/protocol v0.11.2
 	kythe.io v0.0.55
 )
 
 require (
 	bitbucket.org/creachadair/stringset v0.0.9 // indirect
+	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/francoispqt/gojay v1.2.13 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/neelance/parallel v0.0.0-20160708114440-4de9ce63d14c // indirect
+	github.com/opentracing/basictracer-go v1.1.0 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/segmentio/encoding v0.2.7 // indirect
+	github.com/prometheus/client_golang v0.8.0 // indirect
+	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
+	github.com/prometheus/common v0.0.0-20180801064454-c7de2306084e // indirect
+	github.com/prometheus/procfs v0.0.0-20180725123919-05ee40e3a273 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
-	go.lsp.dev/pkg v0.0.0-20210323044036-f7deec69b52e // indirect
-	go.lsp.dev/uri v0.3.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.17.0 // indirect
+	github.com/slimsag/godocmd v0.0.0-20161025000126-a1005ad29fe3 // indirect
+	github.com/sourcegraph/ctxvfs v0.0.0-20180418081416-2b65f1b1ea81 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20210803142424-70bd63adacf2 // indirect
 	google.golang.org/grpc v1.39.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/inconshreveable/log15.v2 v2.0.0-20200109203555-b30bc20e4fd1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
